89b022913e17badee4340ad241779ca099c099bf
NCowley <ncowley8304@student.whatcom.edu>
2020-04-20 19:22:55 -0700

    guess who?

--- src/Guess.java
+++ src/Guess.java
@@ -1,11 +1,25 @@
-/*
-Mariam Khatib
-Cis 140
-lab 4: Guessing
-IDE: intellij
 
-This lab will have the users guess out a randomly generated number
+//Users: Mariam Khatib, "Doc" Nathan Cowley, Kylie Johnson
+//"Non-user program assistant": "Tron"
+//CS 145
+//Lab 1: Guessing Game
+//IDE: IntelliJ
+/*Purpose of Program Summary:
+   This program plays a guessing game. The program will print out an introduction, then
+   it will create a random number between one and the constant, and the player
+   will guess it until they get it correct. If the player guesses incorrectly then
+   the program will tell them they guessed incorrectly, then tell them if the correct answer
+   is higher or lower than their input and then ask for another number. If the answer
+   is correct, then the program will them them the amount of guesses they took to
+   get the right answer, then ask if they would like to play another game. If they
+   respond with y, then the process repeats. If they respond with n, then the program
+   gives them the statistic of the total games the user played, including the number
+   of games played, total guesses over all games, average amount of guesses, and
+   the high score.
  */
+// ©DuchessDocSpacetranautIndustries., All rights Reserved
+// So, Hippity Hoppety: Get OFF our Property!!
+
 import java.util.Scanner;
 import java.util.*;
 
@@ -15,7 +29,9 @@ public class Guess {
         HIGH, LOW, CORRECT;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //This main method calls an initial game and then creates a while loop so that
+                                             //unless the user inputs a phrase that starts with "n", the user will continue to play again.
+                                             //If user inputs a "n" word, main will call "TheResult" method and end.
         GameIntro();
 
         int guess;
@@ -44,7 +60,7 @@ public class Guess {
         TheResult(numguess, numgame,bestgame);
     }
 
-    public static void GameIntro(){
+    public static void GameIntro(){ //Prints a large amount of introductory exposition, helps clean up main.
         System.out.println("This program allows you to play a guessing game.");
         System.out.println("I will think of a number between 1 and");
         System.out.println("100 and will allow you to guess until");
@@ -54,7 +70,11 @@ public class Guess {
         System.out.println();
 
     }
-       public static int TheGame() {
+       public static int TheGame() { //This method creates a random integer between 1 and 100 and will continue to ask
+                                     // for user input of integers until the user can guess the correct one, continually
+                                     // telling him whether his guess is too low or high.
+                                     // It will then tell him the amount of guesses it took the user to guess the
+                                     // correct integer.
            Random random = new Random();
            int num = random.nextInt(100);
            int guess;
@@ -79,6 +99,7 @@ public class Guess {
 
        }
        public static void TheResult(int numguess, int numgame, int bestgame) {
+        //This method displays the total amount of games, guesses, average guess per game, and the best game played.
 
            System.out.println("Total games: " + numgame);
            System.out.println("Total Guesses: " + numguess);
